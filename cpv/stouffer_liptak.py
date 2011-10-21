33fef46c1cc87bcdde13a7b38e67b4753c375142
Brent Pedersen <bpederse@gmail.com>
2011-10-20 20:37:47 -0600

    the matrix stuff requires an explicit shape of (X, 1), not just be (X,)

--- cpv/stouffer_liptak.py
+++ cpv/stouffer_liptak.py
@@ -15,7 +15,8 @@ def stouffer_liptak(pvals, sigma=None):
     >>> stouffer_liptak([0.1, 0.2, 0.8,0.12, 0.11])
     {'p': 0.0497..., 'C': -1.647...}
     """
-    qvals = qnorm(pvals, loc=0, scale=1)
+    L = len(pvals)
+    qvals = qnorm(pvals, loc=0, scale=1).reshape(L, 1)
     # dont do the correction unless sigma is specified.
     if not sigma is None:
         C = chol(sigma)
