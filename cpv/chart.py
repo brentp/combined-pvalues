5bf00838ec321f73fae2cb91bde54fb77ec918bf
Brent Pedersen <bpederse@gmail.com>
2011-10-22 07:06:40 -0600

    auto-adjust chart width

--- cpv/chart.py
+++ cpv/chart.py
@@ -8,10 +8,12 @@ def encode(numbers):
     return "&chd=t:%s" % ','.join(["%.3f" % n for n in numbers])
 
 
-def chart(acfs, base="https://chart.googleapis.com/chart?cht=bvs&chs=550x250",
+def chart(acfs, base="https://chart.googleapis.com/chart?cht=bvs&",
     color="224499"):
     values = [float(acf) for kbin, acf in acfs]
     url = encode(values)
+    chart_width = min(1000, 24 + 50 * len(values))
+    url += "&chs=%ix250" % chart_width
     url += "&chco=" + color
     xlabels = "|".join("%s-%s" % k for k, v in acfs)
     # draw the x-axis ... and the bin-names
@@ -19,5 +21,5 @@ def chart(acfs, base="https://chart.googleapis.com/chart?cht=bvs&chs=550x250",
 
     url += "&chxr=1,0,%.2f" % (max(values) + 0.02)
     url += "&chm=N,000000,0,-1,12"
-    url += "&chbh=40,17,17&chds=a"
+    url += "&chbh=42,6,12&chds=a"
     return base + url
