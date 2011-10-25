69f34286343eace2a7f074e4c4feb64f60a86fd6
Brent Pedersen <bpederse@gmail.com>
2011-10-25 11:28:43 -0600

    allow the sample R* to come from the observed p-values. Seems to give better score.

--- cpv/rpsim.py
+++ cpv/rpsim.py
@@ -17,13 +17,22 @@ from numpy.linalg import cholesky as chol
 qnorm = norm.ppf
 pnorm = norm.cdf
 
-def gen_correlated(sigma, n):
+def gen_correlated(sigma, n, X=None):
     """
     generate autocorrelated data according to the matrix
-    sigma
+    sigma. if X is None, then data will be sampled from
+    the uniform distibution. Otherwise, it will be sampled
+    from X. Where X is then *all* observed
+    p-values.
     """
     sigma = np.asmatrix(sigma)
-    X = np.random.uniform(0, 1, size=(sigma.shape[0], n))
+    if X is None:
+        X = np.random.uniform(0, 1, size=(sigma.shape[0], n))
+    else:
+        idxs = np.random.random_integers(0, len(X) - 1,
+                                         size=sigma.shape[0] * n)
+        X = X[idxs].reshape((sigma.shape[0], n))
+
     return pnorm(chol(sigma) * qnorm(X))
 
 def calc_w(ps, truncate_at):
@@ -31,20 +40,20 @@ def calc_w(ps, truncate_at):
     # avoid underflow by taking log.
     return np.exp(np.sum(np.log(ps[ps <= truncate_at])))
 
-def sim(sigma, ps, nsims, truncate):
+def sim(sigma, ps, nsims, truncate, sample_distribution=None):
     # see: https://gist.github.com/1306786#file_zaykin_truncated.py
     assert isinstance(ps[0], (int, float, long))
     B = 0.
     w0 = calc_w(ps, truncate)
     for i in range(10):
-        Y = gen_correlated(sigma, nsims/10)
+        Y = gen_correlated(sigma, nsims/10, sample_distribution)
         B += sum(calc_w(row, truncate) <= w0 for row in Y.T)
     return B / nsims
 
 def run(args):
     col_num = get_col_num(args.c)
-    #all_ps = np.array([b["p"] for b in bediter(args.pvals, col_num)])
-    rpsim(args.pvals, args.regions, col_num, args.N, args.tau, args.step)
+    rpsim(args.pvals, args.regions, col_num, args.N, args.tau, args.step,
+            args.random)
 
 def _gen_acf(region_info, fpvals, col_num, step):
     # calculate the ACF as far out as needed...
@@ -62,11 +71,12 @@ def _gen_acf(region_info, fpvals, col_num, step):
     print >>sys.stderr, "# Done with one-time ACF calculation"
     return acfs
 
-def rpsim(fpvals, fregions, col_num, nsims, tau, step):
+def rpsim(fpvals, fregions, col_num, nsims, tau, step, random=False):
     piter = chain(bediter(fpvals, col_num), [None])
     prow = piter.next()
     # just use 2 for col_num, but dont need the p from regions.
     region_info = []
+
     for nr, region_line in enumerate((l.rstrip("\r\n")
                                    for l in open(fregions))):
         toks = region_line.split("\t")
@@ -89,11 +99,16 @@ def rpsim(fpvals, fregions, col_num, nsims, tau, step):
 
     acfs = _gen_acf(region_info, (fpvals,), col_num, step)
     # regions first and then create ACF for the longest one.
+    if not random:
+        sample_distribution = np.array([b["p"] for b in bediter(fpvals,
+                                                                col_num)])
+    else:
+        sample_distribution = None
     for region_line, region_len, prows in region_info:
         # gen_sigma expects a list of bed dicts.
         sigma = gen_sigma_matrix(prows, acfs)
         ps = np.array([prow["p"] for prow in prows])
-        psim = sim(sigma, ps, nsims, tau)
+        psim = sim(sigma, ps, nsims, tau, sample_distribution)
         print "%s\t%.4g" % (region_line, psim)
 
 def main():
@@ -104,6 +119,10 @@ def main():
     p.add_argument("-r", dest="regions", help="BED containing all the regions")
     p.add_argument("-t", dest="tau", help="tau cutoff", type=float,
                   default=0.05)
+    p.add_argument("--random", default=False, action="store_true",
+            help="for simulations, the default is to sample from the p-values"
+            " from -p argument; if this flag is set, it will sample from"
+            " the uniform distribution instead")
     p.add_argument("-s", dest="step", type=int, default=50,
             help="step size for acf calculation. should be the same "
             " value as the step sent to -d arg for acf")
