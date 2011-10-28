98505a484f16fc70884f9c6f87b4db75d73e2fb9
Brent Pedersen <bpederse@gmail.com>
2011-10-28 09:19:07 -0600

    correct tail on stouffer-liptak

--- cpv/stouffer_liptak.py
+++ cpv/stouffer_liptak.py
@@ -9,11 +9,21 @@ pnorm = norm.cdf
 def stouffer_liptak(pvals, sigma=None):
     """
     The stouffer_liptak correction.
-    >>> stouffer_liptak([0.1, 0.2, 0.8, 0.12, 0.11])
-    {'p': 0.0497..., 'C': -1.647..., 'OK': True}
+    >>> stouffer_liptak([0.1, 0.2, 0.8, 0.12, 0.011])
+    {'p': 0.0168..., 'C': 2.1228..., 'OK': True}
+
+    >>> stouffer_liptak([0.5, 0.5, 0.5, 0.5, 0.5])
+    {'p': 0.5, 'C': 0.0, 'OK': True}
+
+    >>> stouffer_liptak([0.5, 0.1, 0.5, 0.5, 0.5])
+    {'p': 0.28..., 'C': 0.57..., 'OK': True}
+
+    >>> stouffer_liptak([0.5, 0.1, 0.1, 0.1, 0.5])
+    {'p': 0.042..., 'C': 1.719..., 'OK': True}
     """
     L = len(pvals)
-    qvals = qnorm(pvals, loc=0, scale=1).reshape(L, 1)
+    pvals = np.array(pvals)
+    qvals = qnorm(1 - pvals, loc=0, scale=1).reshape(L, 1)
     # dont do the correction unless sigma is specified.
     result = {"OK": True}
     if not sigma is None:
@@ -21,12 +31,13 @@ def stouffer_liptak(pvals, sigma=None):
             C = chol(sigma)
             Cm1 = np.matrix(C).I # C^-1
             # qstar
-            qvals = Cm1 * np.asmatrix(qvals)
+            qvals = Cm1 * qvals
         except LinAlgError:
             # cant do the correction non-invertible
             result["OK"] = False
     Cp = qvals.sum() / (len(qvals)**0.5)
-    pstar = pnorm(Cp)
+    # get the right tail.
+    pstar = 1 - pnorm(Cp)
     result.update({"C": Cp, "p": pstar})
     return result
 
