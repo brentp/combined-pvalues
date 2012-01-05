726d7a05911b596f68b6df5b5787f09a9295e85e
Brent Pedersen <bpederse@gmail.com>
2012-01-05 11:26:51 -0700

    fix test for new path

--- test.sh
+++ test.sh
@@ -24,7 +24,7 @@ for dist in 1 1000 10000000; do
     test $d -ne 1 && echo "ERROR" $d
 done
 
-./cpv/comb-p.py hist -c 5 cpv/tests/data/pvals.bed > t
+./cpv/comb-p hist -c 5 cpv/tests/data/pvals.bed > t
 
 # unittests.
 python cpv/tests/test.py
