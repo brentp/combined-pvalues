bbaf753f92c359020e922c09829eb7f0b564a817
Brent Pedersen <bpederse@gmail.com>
2012-01-05 11:24:52 -0700

    more info in setup

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 
 from distutils.core import setup
 
-setup(name='combp',
+setup(name='cpv',
       version='0.1',
       description='combine p-values',
       author='Brent Pedersen',
       author_email='bpederse@gmail.com',
+      license='MIT',
       url='https://github.com/brentp/combined-pvalues',
       packages=['cpv', 'cpv.tests'],
-      scripts=['cpv/comb-p']
-     )
+      scripts=['cpv/comb-p'],
+      long_description=open('README.rst').read(),
+      classifiers=["Topic :: Scientific/Engineering :: Bio-Informatics"],
+ )
