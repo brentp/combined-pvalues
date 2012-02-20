3923412569cc0875b4dd8c79e05463e965dfc957
Brent Pedersen <bpederse@gmail.com>
2012-02-19 19:27:10 -0700

    update charm example

--- examples/charm/run.sh
+++ examples/charm/run.sh
@@ -1,3 +1,9 @@
+#BSUB -J combp[4-9]
+#BSUB -R rusage[mem=6]
+#BSUB -e logs/combp.%I.%J.err
+#BSUB -o logs/combp.%I.%J.out
+#BSUB -n 12
+
 # wget http://rafalab.jhsph.edu/data/shores/natgen2009.csv
 # wget http://rafalab.jhsph.edu/data/shores/natgen2009.tgz
 
@@ -6,12 +12,12 @@ R --slave --args data/natgen2009.csv data/methp.txt \
     SampleID quantile < scripts/charm.normalize.R
 sed -i "1s/^/ID\t/" data/methp.txt
 NORMALIZE
-
+<<FIT
 NCOL=2162407
 STEP=8000
-for i in $(awk -v cols=$NCOL -v step=$STEP 'BEGIN{for(i=2;i<cols;i+=step){print i }}'); do
+for i in (awk -v cols=$NCOL -v step=$STEP 'BEGIN{for(i=2;i<cols;i+=step){print i }}'); do
     start=$i
-    end=$(($i + $STEP - 1))
+    end=($i + $STEP - 1)
     nf=data/fit/$start-$end.split
     bedp=data/fit/$start-$end.bed
     cut -f 1,$start-$end data/methp.txt > $nf
@@ -21,4 +27,24 @@ for i in $(awk -v cols=$NCOL -v step=$STEP 'BEGIN{for(i=2;i<cols;i+=step){print 
         -o logs/$start-$end.out \
         -R "rusage[mem=7]"
 done
-     
+
+cat data/fit/*.bed | awk 'NR == 1 || $1 != "#chrom"' \
+    | sort -k1,1 -k2,2n > data/pvalues.bed
+FIT
+
+COLS=(fake chrom start end p.disease p.tissue p.colon p.frontalcortex p.liver p.spleen)
+COL=${COLS[$LSB_JOBINDEX]}
+
+PRE=data/quantile/$COL/$COL
+mkdir -p data/quantile/$COL
+
+comb-p pipeline \
+    -c $LSB_JOBINDEX \
+    -s \
+    --seed 0.0005 \
+    --dist 80 --step 40 \
+    -p $PRE \
+    data/pvalues.bed
+
+awk 'NR == 1 || ($5 > % && $7 < 0.001)' ${PRE}.regions-p.bed \
+    > ${PRE}.sig.regions.bed
