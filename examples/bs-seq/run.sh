b04652a3697d5ec0390f8b63e5ecd6c269422d0d
Brent Pedersen <bpederse@gmail.com>
2012-03-01 10:44:51 -0700

    add script to run bs-seq example

--- examples/bs-seq/run.sh
+++ examples/bs-seq/run.sh
@@ -1,17 +1,17 @@
 #BSUB -J gsnap
 #BSUB -n 12
-#BSUB -e logs/gsnap.err
-#BSUB -o logs/gsnap.out
+#BSUB -e logs/methylcoder.err
+#BSUB -o logs/methylcoder.out
 
-READSA=reads/WT_endosperm
-READSB=reads/WT_embryo
+READSA=reads/2_endosperm
+READSB=reads/2_embryo
 <<DONE
-rm -rf reads/*
-mkdir -p reads/
 
-#1 2 3 4 5
-for i in 2 
-do
+rm -f $READSA*
+rm -f $READSB*
+#mkdir -p reads/
+
+for i in 1 2 3 4 5; do
     wget -O - http://dzlab.pmb.berkeley.edu:8080/work/GEO_submission/raw/WT_endosperm_BS_seq_raw_batch-${i}.1.fastq >> ${READSA}_1.fastq &
     wget -O - http://dzlab.pmb.berkeley.edu:8080/work/GEO_submission/raw/WT_endosperm_BS_seq_raw_batch-${i}.2.fastq >> ${READSA}_2.fastq
     wait
@@ -22,6 +22,8 @@ do
 
 done
 exit;
+DONE
+<<REF
 mkdir -p reference && cd reference
 rm -f thaliana_v10.fasta
 for i in `seq 1 5` C M
@@ -30,10 +32,20 @@ do
 done
 perl -pi -e "s/^>([^\s]+).*/>\1/;tr/C/c/" thaliana_v10.fasta
 
-DONE
+REF
 
+mkdir -p data/$(basename $READSA)
 python gsnap-meth.py \
     -r reference/thaliana_v10.fasta \
-    -t 12 --out-dir data/ \
+    -t 12 --out-dir data/$(basename $READSA) \
     --extra-args "--npaths 1 --quiet-if-excessive" \
-        reads/WT_endosperm_1.fastq reads/WT_endosperm_2.fastq
+    ${READSA}_1.fastq ${READSA}_2.fastq
+    > data/$(basename ${READSA}).meth.txt
+
+mkdir -p data/$(basename $READSB)
+python gsnap-meth.py \
+    -r reference/thaliana_v10.fasta \
+    -t 12 --out-dir data/$(basename $READSB) \
+    --extra-args "--npaths 1 --quiet-if-excessive" \
+    ${READSB}_1.fastq ${READSB}_2.fastq
+    > data/$(basename ${READSB}).meth.txt
