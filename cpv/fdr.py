a7366ad3b3a08c1956c232ed81b430c8ede34fb1
brentp <bpederse@gmail.com>
2012-05-30 14:54:47 -0600

    remove dependency on statsmodels, simplify fdr correction code

--- cpv/fdr.py
+++ cpv/fdr.py
@@ -6,21 +6,23 @@ from _common import bediter
 from itertools import izip
 from _common import get_col_num
 import numpy as np
+import sys
+
+def drop_header(fh):
+    line = fh.readline()
+    if not (line[0] == "#" or line.split()[0] == "chrom"):
+        fh.seek(0)
 
 def run(args):
     # get rid of N, just keep the correlation.
     col_num = get_col_num(args.c)
+    col_null = get_col_num(args.null) if args.null else None
     if args.qvality:
-        col_null = get_col_num(args.null)
         for qval, pep, l in _qvality(args.bed_file, col_num, col_null):
             print "%s\t%.4g\t%4g" % (l.rstrip("\r\n"), qval, pep)
-    elif args.null:
-        col_null = get_col_num(args.null)
+    else:
         for qval, l in obs_fdr(args.bed_file, col_num, col_null):
             print "%s\t%.4g" % (l.rstrip("\r\n"), qval)
-    else:
-        for bh, l in fdr(args.bed_file, col_num):
-            print "%s\t%.4g" % (l.rstrip("\r\n"), bh)
 
 def _qvality(fbed_file, col_num, col_null):
    from qvality import qvality
@@ -32,44 +34,33 @@ def _qvality(fbed_file, col_num, col_null):
    for (pval, pep, qval), l in izip(qvality(ps, nulls, r=None), fh):
        yield qval, pep, l
 
-def obs_fdr(fbed_file, col_num, col_null):
-   ps = [b['p'] for b in bediter(fbed_file, col_num)]
-   nulls = [b['p'] for b in bediter(fbed_file, col_null)]
-   fh = open(fbed_file)
-   drop_header(fh)
-   for qval, l in izip(relative_fdr(ps, nulls), fh):
-       yield qval, l
+def obs_fdr(fbed_file, col_num, col_null=None):
+    ps = [b['p'] for b in bediter(fbed_file, col_num)]
+    if col_null is None:
+        # Benjamini-Hochberg.
+        print >>sys.stderr, "BH"
+        nulls = np.arange(1, len(ps) + 1) / float(len(ps))
+    else:
+        nulls = [b['p'] for b in bediter(fbed_file, col_null)]
+    fh = open(fbed_file)
+    drop_header(fh)
+    for qval, l in izip(relative_fdr(ps, nulls), fh):
+        yield qval, l
 
-# TODO (if null is None: null = 1 / np.arange(len(observed))
 def relative_fdr(observed, null):
     observed = np.asarray(observed)
+
     null = np.asarray(null)
+    null.sort()
 
     obs_sort_ind = np.argsort(observed)
     observed = observed[obs_sort_ind]
     obs_unsort_ind = obs_sort_ind.argsort()
 
-    null.sort()
-
     corrected = observed / null
     corrected = np.minimum.accumulate(corrected[::-1])[::-1]
     corrected[corrected > 1] = 1
     return corrected[obs_unsort_ind]
-
-def drop_header(fh):
-    line = fh.readline()
-    if not (line[0] == "#" or line.split()[0] == "chrom"):
-        fh.seek(0)
-
-def fdr(fbed_file, col_num):
-    from scikits.statsmodels.stats import multicomp as multitest
-    pvals = np.array([b["p"] for b in bediter(fbed_file, col_num)],
-                        dtype=np.float64)
-    bh_pvals = multitest.multipletests(pvals, method='fdr_bh')[1]
-    fh = open(fbed_file)
-    drop_header(fh)
-    for bh, l in izip(bh_pvals, fh):
-        yield bh, l
 
 def main():
     p = argparse.ArgumentParser(description=__doc__,
