cb5802ca439c0629576b6ad8af9e7329d0bc3acc
Brent Pedersen <bpederse@gmail.com>
2012-06-25 13:09:30 -0600

    add 20 shufflings to R script

--- examples/charm/scripts/fit.lm.R
+++ examples/charm/scripts/fit.lm.R
@@ -8,8 +8,13 @@ df$TissueType = factor(df$TissueType)
 df$DiseaseState = factor(df$DiseaseState)
 
 set.seed(42)
-shuff_df = df[sample(1:nrow(df), nrow(df)),]
+for(i in seq(1, 20)){
+    order1 = sample(1:nrow(df), nrow(df))
+    df[,paste("shuff_tissue", i, sep="_")] = df$TissueType[order1]
+    df[,paste("shuff_disease", i, sep="_")] = df$DiseaseState[order1]
 
+    #shuff_df = df[sample(1:nrow(df), nrow(df)),]
+}
 
 for (tiss in c("colon", "frontalcortex", "liver", "spleen")) {
     df[,tiss] = df$TissueType == tiss
@@ -20,8 +25,10 @@ data = read.delim(fname, header=T, sep="\t")
 stopifnot(all(df$SampleID == data$ID))
 names = colnames(data)
 
+
 write(paste("#chrom", "start", "end", "p.disease", "p.tissue", 
-                                      "p.shuff.disease", "p.shuff.tissue",
+    paste("p-shuff-tiss", 1:20, sep="-", collapse="\t"),
+    paste("p-shuff-disease", 1:20, sep="-", collapse="\t"),
  #"p.colon", "p.frontalcortex", "p.liver", "p.spleen", 
         sep="\t"), stdout())
 for (i in 2:ncol(data)) {
@@ -34,17 +41,24 @@ for (i in 2:ncol(data)) {
     p.tissue = format(r["TissueType", "Pr(>F)"], digits=4)
     p.disease = format(r["DiseaseState", "Pr(>F)"], digits=4)
 
+    p.shuff.tissue = rep(NA, 20)
+    p.shuff.disease = rep(NA, 20)
     # shuffled
-    m = lm(methp ~ DiseaseState + TissueType, data=shuff_df)
-    r = drop1(m, ~ ., test="F")
-    p.shuff.tissue = format(r["TissueType", "Pr(>F)"], digits=4)
-    p.shuff.disease = format(r["DiseaseState", "Pr(>F)"], digits=4)
+    for(i in 1:20){
+        m = lm(as.formula(paste("methp ~ shuff_disease_", i, " + shuff_tissue_", i, sep="")), data=df)
+        r = drop1(m, ~ ., test="F")
+        p.shuff.tissue[i] = format(r[paste("shuff_tissue_", i, sep=""), "Pr(>F)"], digits=4)
+        p.shuff.disease[i] = format(r[paste("shuff_disease_", i, sep=""), "Pr(>F)"], digits=4)
+    }
 
 
     chrom_start = unlist(strsplit(names[i], "_", fixed=TRUE))
     start = as.numeric(chrom_start[2])
 
-    write(paste(chrom_start[1], start - 1, start + 50, p.disease, p.tissue, p.shuff.disease, p.shuff.tissue, sep="\t"), stdout())
+    write(paste(chrom_start[1], start - 1, start + 50, p.disease, p.tissue,
+        paste(p.shuff.disease, collapse="\t"), 
+        paste(p.shuff.tissue, collapse="\t")
+        , sep="\t"), stdout())
     #stopifnot(p.asthma == p.drop.asthma)
 
 }
